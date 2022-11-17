8694824b759e05c7c4e4a68b29e51443b53f5eac
eri.gram <ericagramuglia@tdf.edu.ar>
2022-11-16 22:04:32 -0300

    ejercicio JS clase 1 16 de nov 2022

---
+++ CLASE1/main.js
@@ -0,0 +1,8 @@
+// 1 - Crear las funciones Javascript para que a través de inputs se le permita
+// ingresar al usuario un titulo, una url de una imagen y un párrafo.
+// Luego con esa informacion, genere una tarjeta cada vez que presiona un botón.
+
+let titulo = document.getElementById ('titulo');
+let url = document.getElementById ('imagen');
+let texto = document.getElementById ('parrafo')
+
